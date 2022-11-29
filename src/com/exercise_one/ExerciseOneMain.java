6c9385b6d450f395abd86b40e193ac17cb455a67
Matias Pavón <matiaspavon5@hotmail.com>
2022-11-29 17:38:30 -0300

    Exercise 1

---
+++ src/com/exercise_one/ExerciseOneMain.java
@@ -0,0 +1,24 @@
+package com.exercise_one;
+
+public class ExerciseOneMain {
+
+    public static void main(String[] args) {
+        String variableTexto = "Hola";
+        int variableNumerica1 = 123;
+        long variableNumerica2 = 456;
+        float variableNumerica3 = 9.5f;
+        double variableNumerica4 = 5.5;
+        boolean variableBool1 = true;
+        boolean variableBool2 = false;
+
+        System.out.println("Variable String: " + variableTexto);
+        System.out.println("Variable int: " + variableNumerica1);
+        System.out.println("Variable long: " + variableNumerica2);
+        System.out.println("Variable float: " + variableNumerica3);
+        System.out.println("Variable double: " + variableNumerica4);
+        System.out.println("Variable boolean 1: " + variableBool1);
+        System.out.println("Variable boolean 2: " + variableBool2);
+
+    }
+
+}
